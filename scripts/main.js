6e02551afab945c67bd19a88e3cd35ca8f429721
ulysses316 <ulises316@live.com.mx>
2021-02-18 19:57:49 -0600

    Elementos con animaciones de la libreia animate.css

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+function baseURL(){
+    let arrValue = [];
+    let parametro = document.getElementById("pokemon").value;
+    let url = "https://pokeapi.co/api/v2/pokemon/" + parametro;
+
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(myJson) {
+            let element = document.getElementById("poke-table");
+            element.style.display = "block";
+            document.getElementById("image").src = myJson.sprites.front_default;
+            arrValue.push(myJson.name);
+            arrValue.push(myJson.id);
+            arrValue.push(myJson.base_experience);
+            arrValue.push(myJson.height);
+            for (index in arrValue){
+                document.getElementById("parameterValue" + index).innerHTML = arrValue[index];
+            }
+        })
+}       
